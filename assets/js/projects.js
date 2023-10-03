122d3ee30038eca32421f5d60790166fadb8cddb
Olivier Lukas <lukas.olivier@student.howest.be>
2023-10-03 13:53:41 +0200

    adjusted project descriptions & added filtering option

---
+++ assets/js/projects.js
@@ -0,0 +1,35 @@
+const SELECTED = ["school", "personal"];
+
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("click", (e) => {
+    clearProjects();
+    const filter = e.target.value.toLowerCase();
+    if (SELECTED.includes(filter)) {
+      SELECTED.splice(SELECTED.indexOf(filter), 1);
+    } else {
+      SELECTED.push(filter);
+    }
+
+    e.target.classList.toggle("selected");
+    for (const item of SELECTED) {
+      console.log(item);
+      filterProjects(item);
+    }
+  });
+});
+
+function clearProjects() {
+  const projects = document.querySelectorAll(".timeline-container");
+  projects.forEach((project) => {
+    project.classList.add("filtered-out");
+  });
+}
+
+function filterProjects(filter) {
+  const projects = document.querySelectorAll(".timeline-container");
+  projects.forEach((project) => {
+    if (project.innerHTML.toLowerCase().includes(filter)) {
+      project.classList.remove("filtered-out");
+    }
+  });
+}
