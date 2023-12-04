0dfea8de5e4c990d224af28bfc90ddb561ee3069
Olivier Lukas <lukas.olivier@student.howest.be>
2023-12-04 14:38:45 +0100

    added mars project

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -2,21 +2,18 @@ var app = document.querySelector("#home h2");
 
 var typewriter = new Typewriter(app, {
   loop: true,
-  delay: 75,
-  deleteSpeed: 0.1,
+  delay: 25,
+
 });
 
 typewriter
-  .pauseFor(2500)
-  .typeString("Student applied computer science")
-  .pauseFor(500)
-  .deleteAll()
-  .typeString("Musician")
-  .pauseFor(500)
-  .deleteAll()
-  .typeString("Video editor & Logo designer")
-  .pauseFor(500)
-  .deleteAll()
   .typeString("Student applied computer science")
   .pauseFor(1000)
+  .deleteAll()
+  .typeString("Musician")
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Head leader of youth movement 'KSA Izegem'") 
+  .pauseFor(1000)
+  .deleteAll()
   .start();
